7ba9d4cd47153f82dd355289d6263ebd80598352
goberman15 <akbarrmdhn94@gmail.com>
2022-09-25 22:49:55 +0700

    Add SNS Topic and subscription

--- src/handler.ts
+++ src/handler.ts
@@ -4,13 +4,20 @@ import {
   GetCostAndUsageCommandInput,
   Expression,
 } from "@aws-sdk/client-cost-explorer";
+import {
+  SNSClient,
+  PublishCommand,
+  PublishBatchCommandInput,
+  PublishCommandInput,
+} from "@aws-sdk/client-sns";
 
-const client = new CostExplorerClient({});
+const ceClient = new CostExplorerClient({});
+const snsClient = new SNSClient({});
 
 export const hello = async () => {
   const date = new Date();
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate();
 
   const filter: Expression = {
@@ -26,7 +33,7 @@ export const hello = async () => {
     Granularity: "DAILY",
     TimePeriod: {
       Start: `${year}-${month}-${day}`,
-      End: `${year}-${month}-${day+1}`,
+      End: `${year}-${month}-${day + 1}`,
     },
     Metrics: ["BlendedCost"],
     Filter: filter,
@@ -34,9 +41,18 @@ export const hello = async () => {
 
   const command = new GetCostAndUsageCommand(request);
 
-  const result = await client.send(command);
+  const result = await ceClient.send(command);
 
-  if (result.ResultsByTime) {
-    console.log(result.ResultsByTime[0]);
+  if (!result.ResultsByTime) {
+    return;
   }
+
+  const dailyCost = Number(result.ResultsByTime[0].Total?.BlendedCost.Amount);
+
+  const publishInput: PublishCommandInput = {
+    Message: `Total Usage --> US$ ${dailyCost.toFixed(3)}`,
+    TopicArn: process.env.TOPIC_ARN,
+  };
+
+  await snsClient.send(new PublishCommand(publishInput));
 };
