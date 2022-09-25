738d285f7b063e43b68dceb8c180eeb1b6d29c26
goberman15 <akbarrmdhn94@gmail.com>
2022-09-25 20:55:30 +0700

    Add schedule trigger on function

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,42 @@
-export const hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+import {
+  CostExplorerClient,
+  GetCostAndUsageCommand,
+  GetCostAndUsageCommandInput,
+  Expression,
+} from "@aws-sdk/client-cost-explorer";
+
+const client = new CostExplorerClient({});
+
+export const hello = async () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate();
+
+  const filter: Expression = {
+    Not: {
+      Dimensions: {
+        Key: "RECORD_TYPE",
+        Values: ["Credit"],
       },
-      null,
-      2
-    ),
+    },
   };
+
+  const request: GetCostAndUsageCommandInput = {
+    Granularity: "DAILY",
+    TimePeriod: {
+      Start: `${year}-${month}-${day}`,
+      End: `${year}-${month}-${day+1}`,
+    },
+    Metrics: ["BlendedCost"],
+    Filter: filter,
+  };
+
+  const command = new GetCostAndUsageCommand(request);
+
+  const result = await client.send(command);
+
+  if (result.ResultsByTime) {
+    console.log(result.ResultsByTime[0]);
+  }
 };
